a754d40226e712157bf8a26e51f05c17423d69dc
ChenLu <chenlu.lc@hotmail.com>
2020-07-22 17:00:56 -0700

    complete first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,9 @@
+public class HelloNumbers {
+  public static void main(String[] args) {
+    int x = 0; // Data types have to be defined like in C
+    while (x < 10) {
+      System.out.println(x);
+      x++;
+    }
+  }
+}
